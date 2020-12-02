c94820f9f6a1194de2a54df9fea85aab2d096a00
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-12-03 00:30:57 +0900

    SQSをLambdaのイベントトリガーとしてSQSへメッセージが送信されたらDynamoDBへ保存する処理を追加

--- putErrorLog/main.go
+++ putErrorLog/main.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
-type Response events.APIGatewayProxyResponse
+type SQSEvent events.SQSEvent
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
+func Handler(ctx context.Context, sqsEvent SQSEvent) error {
+	var project *string
+	var messageBody *string
+	var status *string
+	for _, message := range sqsEvent.Records {
+		project = message.MessageAttributes["project"].StringValue
+		messageBody = message.MessageAttributes["message"].StringValue
+		status = message.MessageAttributes["status"].StringValue
 	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
+	mySession := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1")},
+	))
+	svc := dynamodb.New(mySession)
+	inputItem := &dynamodb.PutItemInput{
+		TableName: aws.String("faultline"),
+		Item: map[string]*dynamodb.AttributeValue{
+			"project": {
+				S: project,
+			},
+			"message": {
+				S: messageBody,
+			},
+			"status": {
+				S: status,
+			},
 		},
 	}
-
-	return resp, nil
+	svc.PutItem(inputItem)
+	return nil
 }
 
 func main() {
